5f7ad4d3c1c2304b0c4a05dd0849580a1049c8c9
Denys <asked91@gmail.com>
2020-12-10 23:15:31 +0200

    Pagination

--- resources/views/auth/categories/index.blade.php
+++ resources/views/auth/categories/index.blade.php
@@ -40,6 +40,7 @@
             @endforeach
             </tbody>
         </table>
+        {{ $categories->links('pagination::bootstrap-4') }}
         <a class="btn btn-success" type="button"
            href="{{ route('categories.create') }}">Добавить категорию</a>
     </div>
--- resources/views/auth/orders/index.blade.php
+++ resources/views/auth/orders/index.blade.php
@@ -49,5 +49,6 @@
             @endforeach
             </tbody>
         </table>
+        {{ $orders->links('pagination::bootstrap-4') }}
     </div>
 @endsection
